92189de5dcad1dbb6902f66b2931805ca8e3f6d5
amila <amilafk@gmail.com>
2025-07-07 21:53:15 +1000

    - fix timeformatter locale issue

--- src/main/java/org/eatclub/codingchallenge/util/Constants.java
+++ src/main/java/org/eatclub/codingchallenge/util/Constants.java
@@ -1,7 +1,17 @@
 package org.eatclub.codingchallenge.util;
 
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.Locale;
 
 public class Constants {
-    public final static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("h:mm[a]");
+    public final static DateTimeFormatter TIME_FORMATTER = new DateTimeFormatterBuilder()
+        .appendPattern("h:mm")
+        .optionalStart()
+        .appendPattern("a")
+        .optionalEnd()
+        .parseDefaulting(ChronoField.AMPM_OF_DAY, 0)
+        .toFormatter(Locale.US);
 }
+
