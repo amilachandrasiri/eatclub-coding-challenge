1425bf18e38fc10942d5cb321e2c5fc48c423328
amila <amilafk@gmail.com>
2025-07-07 22:09:25 +1000

    - locale added to time formatter

--- src/main/java/org/eatclub/codingchallenge/util/Constants.java
+++ src/main/java/org/eatclub/codingchallenge/util/Constants.java
@@ -1,7 +1,8 @@
 package org.eatclub.codingchallenge.util;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Constants {
-    public final static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("h:mma");
+    public final static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("h:mma").localizedBy(Locale.US);
 }
